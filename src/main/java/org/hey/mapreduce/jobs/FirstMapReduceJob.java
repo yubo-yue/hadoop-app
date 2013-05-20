95de4601add08cb2829c6102da3e16425ded6c44
Yubo.Yue <yubo.yue@163.com>
2013-05-20 11:21:12 +0000

    first word count example

--- src/main/java/org/hey/mapreduce/jobs/FirstMapReduceJob.java
+++ src/main/java/org/hey/mapreduce/jobs/FirstMapReduceJob.java
@@ -2,73 +2,87 @@ package org.hey.mapreduce.jobs;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.KeyValueTextInputFormat;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public final class FirstMapReduceJob extends Configured implements Tool {
 
-        public static class MapClass extends MapReduceBase implements
-                        Mapper<Text, Text, Text, Text> {
-                public void map(Text key, Text value,
-                                OutputCollector<Text, Text> output, Reporter reporter)
-                                throws IOException {
-                        output.collect(value, key);
-                }
-        }
+	public static class TokenizerMapper extends MapReduceBase implements
+			Mapper<Object, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
 
-        public static class Reduce extends MapReduceBase implements
-                        Reducer<Text, Text, Text, Text> {
-                public void reduce(Text key, Iterator<Text> values,
-                                OutputCollector<Text, Text> output, Reporter reporter)
-                                throws IOException {
-                        String csv = "";
-                        while (values.hasNext()) {
-                                if (csv.length() > 0)
-                                        csv += ",";
-                                csv += values.next().toString();
-                        }
-                        output.collect(key, new Text(csv));
-                }
-        }
+		@Override
+		public void map(Object key, Text value,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				output.collect(word, one);
+			}
+		}
+	}
 
-        public int run(String[] args) throws Exception {
-        Configuration conf = getConf();
-        JobConf job = new JobConf(conf, FirstMapReduceJob.class);
-        Path in = new Path(args[0]);
-        Path out = new Path(args[1]);
-        FileInputFormat.setInputPaths(job, in);
-        FileOutputFormat.setOutputPath(job, out);
-        job.setJobName("MyJob");
-        job.setMapperClass(MapClass.class);
-        job.setReducerClass(Reduce.class);
-        job.setInputFormat(KeyValueTextInputFormat.class);
-        job.setOutputFormat(TextOutputFormat.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        job.set("key.value.separator.in.input.line", ",");
-        JobClient.runJob(job);
-        return 0;
-        }
+	public static class IntSumReducer extends MapReduceBase implements
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
 
-        public static void main(String[] args) throws Exception {
-                int res = ToolRunner.run(new Configuration(), new FirstMapReduceJob(), args);
-                System.exit(res);
-        }
+		@Override
+		public void reduce(Text key, Iterator<IntWritable> values,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+
+			int sum = 0;
+
+			while (values.hasNext()) {
+				sum += values.next().get();
+			}
+
+			result.set(sum);
+			output.collect(key, result);
+
+		}
+
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		Configuration conf = getConf();
+		JobConf job = new JobConf(conf, FirstMapReduceJob.class);
+		Path in = new Path(args[0]);
+		Path out = new Path(args[1]);
+		FileInputFormat.setInputPaths(job, in);
+		FileOutputFormat.setOutputPath(job, out);
+		job.setJobName("MyJob");
+		job.setMapperClass(TokenizerMapper.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		JobClient.runJob(job);
+		return 0;
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		int res = ToolRunner.run(new Configuration(), new FirstMapReduceJob(), args);
+		System.exit(res);
+	}
 
 }
